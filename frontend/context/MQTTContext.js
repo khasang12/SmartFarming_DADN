cf9b7839d7242ec75b990166d5aba2293690c3d8
david <davidhuynh0222@gmail.com>
2023-05-02 22:48:46 +0700

    Fix Multiple Connect MQTT when in-out garden

--- frontend/context/MQTTContext.js
+++ frontend/context/MQTTContext.js
@@ -1,23 +1,33 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useRef, useState } from "react";
 import MQTTConnection from "../services/mqttService.service";
 
 export const MQTTContext = createContext();
 
-export const MQTTProvider = ({userName, password, children }) => {
+class Gardens {
+    static list = {}
+}
+
+export const MQTTProvider = ({garden, children }) => {
     
-    // userName = "Potato_Stack"
-    // password = "aio_JZvY63VOPyGgS4WZFaZ6Z5ueDEc2"
 
-    console.log(userName, password);
+    const {name,adaUserName,x_aio_key} = garden
     const [conn, setConn] = useState(undefined);
-
     useEffect(() => {
         async function init() {
-            console.log("Connecting...");
-            if (conn == undefined) {
-                const newClient = new MQTTConnection([], userName, password);
+            if (conn == undefined && !Gardens.list[name]) {
+                console.log("Connecting...");
+                const newClient = new MQTTConnection([], adaUserName, x_aio_key);
                 await newClient.connect();
                 setConn(newClient);
+                Gardens.list[name] = newClient
+            }else {
+                Gardens.list[name].client.disconnect();
+                delete Gardens.list[name];
+                console.log(Gardens.list);
+                const newClient = new MQTTConnection([], adaUserName, x_aio_key);
+                await newClient.connect();
+                setConn(newClient);
+                Gardens.list[name] = newClient
             }
         }
         init();
